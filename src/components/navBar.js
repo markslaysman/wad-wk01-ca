890ebeeb1e19acd231e6ab305160527f59060206
Mark D. Slaysman <mark_slaysman@yahoo.com>
2022-02-11 13:39:05 -0700

    Project Clean up.  Added NavBar

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -2,6 +2,18 @@ import React from 'react';
 
 export default class NavBar extends React.Component {
     render() {
-        
+        return (
+            <div>
+                <header>
+                    <nav class="my-navbar">
+                        <ul>
+                            <li class="active"><a href="#">Main Page</a></li>
+                            <li><a href="#">Page 2</a></li>
+                            <li><a href="#">Page 3</a></li>
+                        </ul>
+                    </nav>
+                </header>
+            </div>
+        );        
     }
 }
