2f4c220588f99c786c857d6a1de871aa87d85194
Mark D. Slaysman <mark_slaysman@yahoo.com>
2022-02-11 13:44:14 -0700

    Cleaning up code

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import TextInputField from './textInputField';
-import SecuredInputField from './securedInputField'
 
 export default class LoginForm extends React.Component {
     render() {
@@ -10,11 +8,12 @@ export default class LoginForm extends React.Component {
                     <h3 className="cont-item">Log In</h3>
                     <div className="row cont-item">
                         <label>Username: </label>
-                        <TextInputField />
+                        <input id="password-field" type="password" placeholder="Password"></input>
                     </div>
                     <div className="row cont-item">
                         <label>Password: </label>
-                        <SecuredInputField />
+                        <input type="text" id="username-field" placeholder="User Name"></input>
+                        
                     </div>
                 </div>
             </div>
--- src/components/securedInputField.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default class SecuredInputField extends React.Component {
-    render() {
-        return <input id="password-field" type="password" placeholder="Password"></input>
-    }
-}
